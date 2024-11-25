813143631e96855a5a25790391e56df3e8e19392
Rehman047 <u2024026@giki.edu.pk>
2024-11-25 14:20:19 +0500

    Added Feature of Deletion of records

--- sp.cpp
+++ sp.cpp
@@ -7,8 +7,12 @@ struct Student{
 
 
 };
+
+//Glpbal Vars
     int i=0;
     Student store[100];
+    int drecords[100];
+    int ddd=0;
 void add(){
     Student S1;
     cout<<"Enter the name of student : ";
@@ -24,24 +28,47 @@ void print(){
     if (i==0){
         cout<<"No record added yet.";
         return;
-    }
+    }else{
+   
     cout<<"Reg Number | \tName\n";
     
     for(int j=0;j<=i-1;j++){
+        
+
+     if(j<=ddd){
+    if(store[j].reg_no==drecords[j]){
+     cout<<"Record just Deleted!\n";
+     continue;}}
+     
     cout<<store[j].reg_no<<"    | \t"<<store[j].name<<endl;
     
     
     }
+    }   }
+
+void deletez(int &ddd){
+    cout<<"Enter the REG.No ( Only last three digits ) :  ";
+    int del;
+    cin>>del;
+    if(del>i){
+        cout<<"Invalid REG . No";
+    }
+    del+=2025000;
+    drecords[ddd]=del;
+    ddd++;
+    cout<<"Record Deleted! ";
+
 }
+
 int main(){
 
 
 char in;
 do {
 cout<<"\nWelcome to the Student Database Management System\nEnter A to add record of student\n";
-cout<<"Enter P to see all student records\nEnter E to exit\n: ";
+cout<<"Enter P to see all student records\nEnter D to delete record\nEnter E to exit\n: ";
 cin>>in;
-while(in!='A' && in !='P' && in!='E'){
+while(in!='A' && in !='P' && in!='E' && in!='D'){
     cout<<"Enter valid character : ";
     cin>>in;
 }
@@ -52,6 +79,8 @@ else if(in=='P')
 print();
 else if (in=='E')
 return 0;
+else if(in=='D')
+deletez(ddd);
 else
 cout<<"Invalid";
 } while (in!='E');
