9e3f7cc19ff8fee5851b44bb8c829a70590d2bb3
Rehman047 <u2024026@giki.edu.pk>
2024-12-08 14:55:01 +0500

    Added Feature to edit record. User must first input valid reg number then name of field to change like , name or contact.

--- sp.cpp
+++ sp.cpp
@@ -62,6 +62,59 @@ void add(){
     i++;
     cout<<"Record added successfully !!! ";
 }
+void edit(){
+    if(i==0){
+        cout<<"No record added yet.";
+        return;
+    } else{
+        int regn;
+        int edch;
+        string inp;
+        cout<<"Enter Registration Number : ";
+        cin>>regn;
+        while(regn>i || regn==0){
+            cout<<"Invalid Reg. Number. Enter Again : ";
+            cin>>regn;
+        }
+        cout<<"Enter the name of field to change : ";
+        cin>>inp;
+        while(inp!="Name"&& inp!="Contact"){
+            cout<<"Invalid Field. Enter again : ";
+            cin>>inp;
+        }
+
+        regn+=2025000;
+      for(int j=0;j<=i-1;j++){
+        
+       edch=1;
+    for(int z=0;z<=ddd;z++){
+    if(store[j].reg_no==drecords[z]){
+     cout<<"Record just Deleted!\n";
+    edch=0;
+     }
+    }
+    if(edch){
+
+        if(store[j].reg_no==regn){
+            regn-=2025000;
+            regn-=1;
+            if(inp=="Name"){
+    cout<<"Enter the name of Student : ";
+    cin>>store[regn].name;
+    cout<<"Record edited successfully !!! ";
+    }else if(inp=="Contact"){
+    cout<<"Enter the Student's contact number : ";
+    cin>>store[regn].phone;
+    while(digcheck(store[regn].phone)!=3){
+        cout<<"Phone Number should be 3 digits only\nEnter Phone Number Again : ";
+        cin>>store[regn].phone;}
+        cout<<"Record edited successfully !!! ";
+        }}
+   }
+    
+    }
+    }
+}
 void print(){
     
     if (i==0){
@@ -107,7 +160,7 @@ void search(){
     cout<<"Enter Reg Number only last three digits : ";
     cin>>regno;
     if(digcheck(regno)>3){
-        while(digcheck(regno)>3){
+        while(digcheck(regno)>3 || regno>i ||regno==0){
         cout<<"Invalid Reg Number. Enter Again : ";
         cin>>regno;}
     } 
@@ -198,7 +251,7 @@ void del_rec(){
 void options(){
 cout<<"\nEnter A to add record of student\n";
 cout<<"Enter P to see all student records\nEnter D to delete record\n";
-cout<<"Enter S to search for a specific student's record\nEnter X to see all deleted records\n";
+cout<<"Enter S to search for a specific student's record\nEnter X to see all deleted records\nEnter R to edit record\n";
 cout<<"Enter I to install information\nEnter E to exit";
 }
 
@@ -210,8 +263,8 @@ lineprint();
 cout<<"\nWelcome to the Student Database Management System !!!\nEnter O to view available options for input\n";
 cout<<"Enter Input : ";
 cin>>in;
-while(in!="A" && in !="P" && in!="E" && in!="D" && in!="S"&& in!="I" &&in!="X"&&in!="O"){
-    cout<<"Enter valid character Amongst A,P,E,S,I,D,X,O : ";
+while(in!="A" && in !="P" && in!="E" && in!="D" && in!="S"&& in!="I" &&in!="X"&&in!="O"&&in!="R"){
+    cout<<"Enter valid character Amongst A,P,E,S,I,D,X,O,R : ";
     cin>>in;
 }
 
@@ -231,6 +284,8 @@ else if(in=="X")
 del_rec();
 else if(in=="O")
 options();
+else if(in=="R")
+edit();
 else
 cout<<"Invalid";
 } while (in!="E");
